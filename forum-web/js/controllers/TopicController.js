a289975c37dd23bbf9efda059b06696eee27e0ac
SamyuktaPatil <spatil@fnakave.com>
2015-10-20 16:09:33 -0700

    remove default topicType

--- forum-web/js/controllers/TopicController.js
+++ forum-web/js/controllers/TopicController.js
@@ -4,7 +4,6 @@ topicModule.controller("TopicController", ["$scope", "$sce", "$timeout", "$route
 function initTopicController($scope, $sce, $timeout, $routeParams,networkService,TopicService, CommentService, facebookService, UserInfoService, URIHelper,RegistrationService)
 {
 	TopicService.setTopicId($routeParams.topicID);
-	$scope.topicType = "default";
 	$scope.innerButtonTapped = false;
 	if((UserInfoService.isPeelUser() == true))
 		$scope.isPeelUser = true;
