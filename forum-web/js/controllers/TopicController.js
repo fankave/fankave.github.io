c1e03a9a59eed19f10082245b434651dbbc5d719
SamyuktaPatil <spatil@fnakave.com>
2015-10-28 15:56:05 -0700

    pageView Track

--- forum-web/js/controllers/TopicController.js
+++ forum-web/js/controllers/TopicController.js
@@ -3,6 +3,7 @@ topicModule.controller("TopicController", ["$scope", "$sce", "$timeout", "$route
 
 function initTopicController($scope, $sce, $timeout, $routeParams,networkService,TopicService, CommentService, facebookService, UserInfoService, URIHelper,RegistrationService)
 {
+	ga('send', 'pageview', '/topic+$routeParams.topicID');
 	TopicService.setTopicId($routeParams.topicID);
 	$scope.topicType = "livegame";
 	$scope.innerButtonTapped = false;
