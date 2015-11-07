a4f5a660801b0f9bfc6c09d90e4732efc77b8e69
SamyuktaPatil <spatil@fnakave.com>
2015-11-06 20:37:03 -0800

    update dev URL

--- forum-web/js/services/FacebookService.js
+++ forum-web/js/services/FacebookService.js
@@ -6,7 +6,7 @@ function initFacebookController($scope, $routeParams, $http, $compile, facebookS
 	// console.log("initFacebookController");
 	if(HOST_NAME == undefined)
 		HOST_NAME = window.location.host;
-	if(HOST_NAME == 'dev.fanakve.com')
+	if(HOST_NAME == 'dev.fankave.com')
 	REGISTER_SERVER_URI = 'http://dev.fankave.com/v1.0/user/register';
 	$scope.loginToFacebook = function()
 	{
