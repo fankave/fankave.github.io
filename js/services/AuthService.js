ef7ae3def55b72cc05e882ad92603cb4c186eaa7
Robby Hays <rhaysvm42@gmail.com>
2015-11-11 10:29:46 -0800

    Refactors AuthService to general methods

--- js/services/AuthService.js
+++ js/services/AuthService.js
@@ -1,9 +1,6 @@
 authModule.factory("AuthService", ["$http", "UserInfoService", "TopicService", "ReplyService", "networkService","ForumDeviceInfo", "ChannelService",
   function ($http, UserInfoService, TopicService, ReplyService, networkService, ForumDeviceInfo, ChannelService) {
 
-  // FACEBOOK AUTH SECTION SRVC
-  var userLoggedInToFacebook = false; //is this needed at all?
-
   var loginToFacebook = function() {
     FB.login(function (response) {
       if (response.status === 'connected') {
@@ -13,73 +10,81 @@ authModule.factory("AuthService", ["$http", "UserInfoService", "TopicService", "
     });
   };
 
-  var registerFacebookUser = function(response) {
-    
+  var setRegistrationParams = function (type, utcOffset, userData) {
     var deviceId = ForumDeviceInfo.getDeviceId();
-    var facebookData = {};
-    facebookData.id = response.authResponse.userID;
-    facebookData.accessToken = response.authResponse.accessToken;
-
-    var registrationParameters = {
-      "type": "facebook",
+    var registerParams = {
+      "type": type,
       "locale": "en_US",
-      "utcOffset": -25200,
+      "utcOffset": utcOffset,
       "deviceType": "web",
       "deviceId": deviceId,
       "deviceModel": "browser",
       "appKey": "testKey",
-      "appVersion": "0.1",
-      "facebook": facebookData
+      "appVersion": "1.0"
     };
 
+    if (type === 'facebook'){
+      registerParams.facebook = {
+        "id": userData.authResponse.userID,
+        "accessToken": userData.authResponse.accessToken
+      };
+    } else if (type === 'peel'){
+      registerParams.peel = {
+        "id": userData.id,
+        "name": userData.userName
+      }; 
+    }
+    return registerParams;
+  };
+
+  var registerUser = function(registerParams) {
     // Post request to our api to register/retrieve user
-    $http.post(REGISTER_SERVER_URI, JSON.stringify(registrationParameters))
+    $http.post(REGISTER_SERVER_URI, JSON.stringify(registerParams))
       .then(function (response) {
         if (response.status === 200) {
-          userLoggedInToFacebook = true;
+          console.log("Successfully Registered User of Type: " + registerParams.type);
+          if (registerParams.type === 'facebook'){
+            var userLoggedInToFacebook = true;
+          }
 
           // Store user credentials in Local Storage
           UserInfoService.setUserCredentials(
             response.data.userId, 
             response.data.accessToken, 
             response.data.sessionId,
-            "facebook");
+            registerParams.type);
 
-          // Initialize Network Service and determine what type of resource is being accessed
-          networkService.init();
-          if (ReplyService.getPostId() !== undefined) {
-            console.log("found post ID: " + ReplyService.getPostId());
-            window.location = "#/post/" + ReplyService.getPostId();
-          }
-          else if (TopicService.getTopicId() !== undefined) {
-            console.log("found Topic ID: " + TopicService.getTopicId());
-            window.location = "#/topic/" + TopicService.getTopicId();
-          }
-          else if (ChannelService.getChannel() !== undefined) {
-            console.log("found channel ID: " + ChannelService.getChannel());
-            networkService.send(ChannelService.getLiveGameTopic());
-          }
-        }
-      },
+        },
       function (response) {
-        console.log('error');
-        console.log('response:  ' + JSON.stringify(response));
-        console.log('response.status: ' + response.status);
-        console.log('response.data: ' + JSON.stringify(response.data));
-        console.log('response.headers: ' + response.headers);
-        console.log('response.config: ' + response.config);
-        console.log('response.statusText: ' + response.statusText);
-      });
-
+        console.log('Registration Error: ', response);
+      }
+    });
   };
 
-  //FUTURE PEEL AUTH SRVC
-  //TODO...
+  var initializeContent = function() {
+    // Initialize Network Service and determine what type of resource is being accessed
+    networkService.init();
+
+    if (ReplyService.getPostId() !== undefined) {
+      console.log("found post ID: " + ReplyService.getPostId());
+      window.location = "#/post/" + ReplyService.getPostId();
+    }
+    else if (TopicService.getTopicId() !== undefined) {
+      console.log("found Topic ID: " + TopicService.getTopicId());
+      window.location = "#/topic/" + TopicService.getTopicId();
+    }
+    else if (ChannelService.getChannel() !== undefined) {
+      console.log("found channel ID: " + ChannelService.getChannel());
+      networkService.send(ChannelService.getLiveGameTopic());
+    }
+  };
 
   return {
     loginToFacebook: loginToFacebook,
-    registerFacebookUser: registerFacebookUser,
+    setRegistrationParams: setRegistrationParams,
+    registerUser: registerUser,
+    initializeContent: initializeContent,
     userLoggedInToFacebook: userLoggedInToFacebook
-  }
+  };
 
 }]);
