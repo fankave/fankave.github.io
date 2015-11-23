c21332eacbb7870f621fbbb2be7f610c90e8b843
Robby Hays <rhaysvm42@gmail.com>
2015-11-22 22:31:56 -0800

    Tabs UI: 3 Column Header

--- js/controllers/MediaController.js
+++ js/controllers/MediaController.js
@@ -2,8 +2,8 @@
 
 var mediaModule = angular.module('MediaModule', ['angularFileUpload', 'NetworkModule'])
 
-mediaModule.controller('MediaController', ['$scope', '$routeParams', '$window', 'FileUploader', 'MUService', 'UserInfoService', 'networkService', 'CommentService',
-  function ($scope, $routeParams, $window, FileUploader, MUService, UserInfoService, networkService, CommentService) {
+mediaModule.controller('MediaController', ['$scope', '$routeParams', '$window', 'FileUploader', 'MUService', 'UserInfoService', 'networkService', 'CommentService', 'URIHelper',
+  function ($scope, $routeParams, $window, FileUploader, MUService, UserInfoService, networkService, CommentService, URIHelper) {
   
   var UPLOAD_URL = '/v1.0/media/upload';
 
@@ -30,6 +30,68 @@ mediaModule.controller('MediaController', ['$scope', '$routeParams', '$window', 
     $scope.commentText = "";
     $window.location = "#/topic/" + $scope.topicID;
   };
+
+  $scope.setUI = function() {
+    if($scope.isPeelUser === true) {
+      document.getElementById('mediaSection').style.paddingTop = "52px";
+      document.getElementById('header').style.height = "52px"; 
+    } else {
+      document.getElementById('mediaSection').style.paddingTop = "0px";
+      document.getElementById('header').style.height = "0px";
+    }
+  };
+
+  if (UserInfoService.isPeelUser()) {
+    $scope.isPeelUser = true;
+    $scope.setUI();
+  } else {
+    $scope.isPeelUser = false;
+    $scope.setUI();
+  }
+
+  $scope.peelClose = function() {
+    ga('send', 'event', 'Peel', 'click', 'BackToPeelHome');
+    console.log("peelClose()");
+    window.location = "peel://home";
+  };
+
+  $scope.peelWatchOnTV = function() {
+    ga('send', 'event', 'Peel', 'click', 'PeelWatchOnTV');
+    console.log("peelWatchOnTV()");
+    var showId = URIHelper.getPeelShowId();
+    console.log("Peel show on TV uri :  "+ "peel://tunein/"+showId);
+    if(showId !== undefined)
+      window.location = "peel://tunein/"+showId;
+    else
+      window.location = "peel://home";
+  };
+
+  $scope.progressFill = function(progress) {
+
+    return {
+      'width': progress + '%',
+      'background-color': 'rgb(59,166,252)'
+    }
+  };
+
+  $scope.chatSelected = 'selectedTab';
+  $scope.switchTabs = function(tab) {
+    if (tab === 'chat'){
+      $scope.chatSelected = 'selectedTab';
+      $scope.videoSelected = '';
+      $scope.socialSelected = '';
+    }
+    if (tab === 'video'){
+      $scope.chatSelected = '';
+      $scope.videoSelected = 'selectedTab';
+      $scope.socialSelected = '';
+    }
+    if (tab === 'social'){
+      $scope.chatSelected = '';
+      $scope.videoSelected = '';
+      $scope.socialSelected = 'selectedTab';
+    }
+  }
 
   // FILTERS
 
