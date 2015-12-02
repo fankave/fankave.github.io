6282e86eda78900890c35b00d8b8bca0031aeeee
Robby Hays <rhaysvm42@gmail.com>
2015-12-01 19:34:05 -0800

    Mobile User Input Padding

--- js/controllers/TopicController.js
+++ js/controllers/TopicController.js
@@ -256,7 +256,7 @@ function initTopicController($scope, $sce, $window, $sanitize, $timeout, $routeP
     $scope.init();
 
     if ($scope.mobileBrowser === true){
-      document.getElementById('topicSection').style.paddingBottom = "54px";
+      document.getElementById('topicSection').style.marginBottom = "54px";
     }
 
     $scope.$watch("commentsArray", function (newValue, oldValue)
