102dd64d92d9aa75d618e2d062df670a1f9ac208
Robby Hays <rhaysvm42@gmail.com>
2015-12-15 14:14:49 -0800

    Preview Placeholder Image for Video Uploads

--- js/controllers/UserInputController.js
+++ js/controllers/UserInputController.js
@@ -39,7 +39,7 @@ angular.module("UserInput", ["NetworkModule","TopicModule","MediaModule","angula
         console.log('F:', f);
 
         if (!f.type.match('image.*')) {
-          return;
+          var previewSrc = "img/videoPrevIcon@2x.png";
         }
 
         var reader = new FileReader();
@@ -47,7 +47,7 @@ angular.module("UserInput", ["NetworkModule","TopicModule","MediaModule","angula
           return function (e) {
             var span = document.createElement('span');
             span.innerHTML = ['<img class="thumb" src="',
-              e.target.result,
+              previewSrc || e.target.result,
               '"/>'].join('');
             if ($scope.mobileBrowser === true && !dontAdd){
               document.getElementById('mobilePreview').insertBefore(span, null);
@@ -68,6 +68,7 @@ angular.module("UserInput", ["NetworkModule","TopicModule","MediaModule","angula
         e.wrap('<form>').closest('form').get(0).reset();
         e.unwrap();
         dontAdd = false;
+        _this.unhighlightPost();
       };
 
       // CALLBACKS
@@ -86,8 +87,7 @@ angular.module("UserInput", ["NetworkModule","TopicModule","MediaModule","angula
       };
       this.uploader.onAfterAddingFile = function(fileItem) {
         console.info('onAfterAddingFile', fileItem);
-
-
+        _this.highlightPost();
       };
       this.uploader.onAfterAddingAll = function(addedFileItems) {
         console.info('onAfterAddingAll', addedFileItems);
