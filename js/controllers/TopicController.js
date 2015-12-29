76942225ef316e50afa8a9e71d2066e74fe1be18
Robby Hays <rhaysvm42@gmail.com>
2015-12-29 14:40:59 -0800

    Embed Directive

--- js/controllers/TopicController.js
+++ js/controllers/TopicController.js
@@ -195,7 +195,10 @@ function initTopicController($scope, $sce, $window, $location, $sanitize, $timeo
           tempComment.embed = commentsdata[i].embed;
           tempComment.embed.embedCreatedAt = commentsdata[i].embedCreatedAt;
           tempComment.embed.embedCreatedAtFull = commentsdata[i].embedCreatedAtFull;
-          tempComment.embed.media = commentsdata[i].embed.media[0];
+          
+          if (tempComment.embed.type === 'media'){
+            tempComment.embed.media = commentsdata[i].embed.media[0];
+          }
 
           if (tempComment.providerName === "Twitter"){
             tempComment.embed.embedLogo = "img/twitterLogo@2x.png";
