9cf5b4a1491ac7f9f68921d14ffe69fc0dec63f6
Robby Hays <rhaysvm42@gmail.com>
2015-12-29 15:17:48 -0800

    Separate Directives into Separate Folder/File

--- js/controllers/TopicController.js
+++ js/controllers/TopicController.js
@@ -565,27 +565,3 @@ function initTopicController($scope, $sce, $window, $location, $sanitize, $timeo
 
 };
 
-topicModule.directive('repeatFinishedNotify', function () {
-  return function (scope, element, attrs) {
-    if (scope.$last){
-      // scope.scrollToBookmark();
-      console.log("DONE LOADING COMMENTS");
-      scope.hideLoading();
-      scope.setLinksOnComments();
-      scope.setDocVars();
-    }
-  };
-});
-
-topicModule.directive('embedSharedContent', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      thisPost: '=embedPost',
-      imageZoom: '&',
-      trustSource: '&',
-      preventNav: '&'      
-    },
-    templateUrl: '/partials/shared.html'
-  };
-});
