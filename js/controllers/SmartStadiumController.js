77fffae5596b8ab5a6a77fb94c95b1db6e08ffa4
Robby Hays <rhaysvm42@gmail.com>
2016-01-05 17:26:16 -0800

    Background Images

--- js/controllers/SmartStadiumController.js
+++ js/controllers/SmartStadiumController.js
@@ -7,25 +7,25 @@ smartSModule.controller("SmartStadiumController", ["$scope", function ($scope){
 
   this.viewHighlights = function() {
     _this.ssActiveScreen = 'highlights';
-    $('#ssStaticContainer').css('background-image','/img/SS/ss_highlights@3x.png');
+    $('#ssStaticContainer').css('background-image','url(img/SS/ss_highlights@3x.png)');
     _this.viewScreen = true;
   };
 
   this.viewRoster = function() {
     _this.ssActiveScreen = 'roster';
-    $('#ssStaticContainer').css('background-image','/img/SS/ss_roster@3x.png');
+    $('#ssStaticContainer').css('background-image','url(img/SS/ss_roster@3x.png)');
     _this.viewScreen = true;
   };
 
   this.viewStats = function() {
     _this.ssActiveScreen = 'stats';
-    $('#ssStaticContainer').css('background-image','/img/SS/ss_stats@3x.png');
+    $('#ssStaticContainer').css('background-image','url(img/SS/ss_stats@3x.png)');
     _this.viewScreen = true;
   };
 
   this.viewProfileStats = function() {
     _this.ssActiveScreen = 'profileStats';
-    $('#ssStaticContainer').css('background-image','/img/SS/ss_profile_stats@3x.png');
+    $('#ssStaticContainer').css('background-image','url(img/SS/ss_profile_stats@3x.png)');
     _this.viewScreen = true;
   };
 
