74a9927ccc04b0c6f9e1b9653831a298b333524b
Robby Hays <rhaysvm42@gmail.com>
2016-01-05 17:36:26 -0800

    BG Images

--- js/controllers/SmartStadiumController.js
+++ js/controllers/SmartStadiumController.js
@@ -8,25 +8,21 @@ smartSModule.controller("SmartStadiumController", ["$scope", function ($scope){
   this.viewHighlights = function() {
     _this.ssActiveScreen = 'highlights';
     _this.viewScreen = true;
-    $('#ssStaticContainer').css('background-image','url(img/SS/ss_highlights@3x.png)');
   };
 
   this.viewRoster = function() {
     _this.ssActiveScreen = 'roster';
     _this.viewScreen = true;
-    $('#ssStaticContainer').css('background-image','url(img/SS/ss_roster@3x.png)');
   };
 
   this.viewStats = function() {
     _this.ssActiveScreen = 'stats';
     _this.viewScreen = true;
-    $('#ssStaticContainer').css('background-image','url(img/SS/ss_stats@3x.png)');
   };
 
   this.viewProfileStats = function() {
     _this.ssActiveScreen = 'profileStats';
     _this.viewScreen = true;
-    $('#ssStaticContainer').css('background-image','url(img/SS/ss_profile_stats@3x.png)');
   };
 
   this.exitView = function() {
