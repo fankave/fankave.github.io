4fdd8d3ff7740498cea7d5b6c9a6ea25a65b845f
Robby Hays <rhaysvm42@gmail.com>
2016-01-09 17:44:22 -0800

    Android Input

--- js/controllers/UserInputController.js
+++ js/controllers/UserInputController.js
@@ -171,8 +171,8 @@ angular.module("UserInput", ["NetworkModule","TopicModule","MediaModule","angula
         var viewport = document.querySelector("meta[name=viewport]");
         var viewportAlt = $('meta[name=viewport]');
         // viewport.setAttribute('content', 'height=auto');
-        var viewHeight = viewport.height();
-        var viewHeightAlt = viewportAlt.height();
+        // var viewHeight = viewport.height();
+        // var viewHeightAlt = viewportAlt.height();
         console.log("Viewports: ", viewport,viewportAlt,viewHeight,viewHeightAlt);
       }
 
