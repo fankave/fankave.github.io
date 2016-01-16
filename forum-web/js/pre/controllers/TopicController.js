20444ab74620aacd9e412a06b901a9ffe9753f22
Robby Hays <rhaysvm42@gmail.com>
2016-01-15 18:48:19 -0800

    JS Pre-Load Scripts

---
+++ forum-web/js/pre/controllers/TopicController.js
@@ -0,0 +1,584 @@
+angular.module("TopicModule", ["NetworkModule", "SplashModule", "AuthModule", "MediaModule", "angularFileUpload","SocialModule"])
+.controller("TopicController", ["$scope", "$sce", "$window", "$location","$sanitize", "$timeout", "$routeParams","networkService", "TopicService","CommentService", "UserInfoService","URIHelper","AuthService","SplashService","MUService","ForumStorage","FileUploader","SocialService","ChannelService","UserAgentService",
+
+function ($scope, $sce, $window, $location, $sanitize, $timeout, $routeParams,networkService,TopicService, CommentService, UserInfoService, URIHelper, AuthService, SplashService,MUService,ForumStorage,FileUploader,SocialService, ChannelService, UserAgentService)
+{
+  var lastComment = false;
+  // Check For Mobile Browser
+  if (UserAgentService.isMobileUser()){
+    $scope.mobileBrowser = true;
+    $scope.mobileUserAgent = UserAgentService.getMobileUserAgent();
+    console.log("MOBILE USER AGENT: ", $scope.mobileUserAgent);
+  } else {
+    $scope.mobileBrowser = false;
+  }
+
+  if (!$scope.commentsArray){
+    $scope.loadingChat = true;
+  }
+
+  ga('send', 'pageview', "/topic/"+$routeParams.topicID);
+  console.log('Sent Pageview from /topic/' + $routeParams.topicID);
+  
+  TopicService.setTopicId($routeParams.topicID);
+  $scope.topicType = "livegame";
+  $scope.innerButtonTapped = false;
+  if (UserInfoService.isSmartStadiumUser()){
+    $scope.isSmartStadiumUser = true;
+    // if (!UserInfoService.hasUserVisited()){
+      console.log('SS USER HASNT VISITED');
+      $scope.hideSSSplash = false;
+      ForumStorage.setToLocalStorage("hasUserVisited", true);
+      $timeout(function() {$scope.continueToExperience('smartS'); }, 5000);
+    // }
+  }
+  else if (UserInfoService.isMI16User()){
+    $scope.isMI16User = true;
+  }
+  else if(UserInfoService.isPeelUser()){
+    $scope.isPeelUser = true;
+    if (!UserInfoService.hasUserVisited()){
+      console.log('PEEL USER HASNT VISITED');
+      $scope.hidePeelSplash = false;
+      ForumStorage.setToLocalStorage("hasUserVisited", true);
+      $timeout(function() {$scope.continueToExperience('peel'); }, 5000);
+    }
+  }
+  else {
+    $scope.isPeelUser = false;  
+    $scope.hidePeelSplash = true;
+    $scope.hideSSSplash = true;
+  }
+//  var tempJasonNFL = {};
+//  
+//  
+//  console.log("Team Names");
+//  for(i=0;i<tempJasonNFL.results.length;i++){
+//    console.log( "http://was.fankave.com/forum/#/channel/"+tempJasonNFL.results[i].channelId);
+//  }
+  //Samyukta test
+//  $(window).scroll(function(){
+//      $("#textInputFieldTopic").css("top", Math.max(160, 250 - $(this).scrollTop()));
+//  });
+  // $scope.hidePeelSplash = true;
+
+  $scope.continueToExperience = function(env) {
+    console.log("CONTINUE XP CLICKED");
+    if (env === 'peel'){
+      SplashService.setPeelSplash(true);
+      $scope.hidePeelSplash = true;
+    } else if (env === 'smartS'){
+      SplashService.setSSSplash(true);
+      $scope.hideSSSplash = true;
+    }
+  };
+  $scope.setScoreCardUI = function() {
+    if ($scope.topicType === 'livegame'){
+      if ($scope.isPeelUser){
+        $('#topicSection').css('padding-top','54px');
+      } else if ($scope.isSmartStadiumUser){
+        $('#topicSection').css('padding-top','54px');
+      } else if ($scope.isMI16User){
+        // $('#topicSection').css('padding-top','54px');
+      } else {
+        $('#topicSection').css('padding-top','0px');
+      }
+    }
+  };
+  
+  var updateTopic = function(){
+    if(TopicService.getTopic() !== undefined){
+      $scope.topicType = TopicService.getTopicType();
+      if(TopicService.isWatchingTopic() === false){
+        networkService.send(TopicService.getFollowChannelRequest());
+        networkService.send(TopicService.watchTopicRequest($routeParams.topicID));
+      }
+      
+      
+      $scope.setScoreCardUI();
+      if($scope.topicType == "livegame"){
+        console.log("Inside topic set :"+ TopicService.getTeamA());
+        //Score API update
+        $scope.leftTeam = TopicService.getTeamA();
+        $scope.rightTeam = TopicService.getTeamB();
+        var score = TopicService.getScore();
+        if(score != undefined){
+          $scope.leftTeamScore = score.points[0];
+          $scope.rightTeamScore = score.points[1];
+        }
+        $scope.gameStatus = TopicService.getGameStatus();
+        // console.log($scope.gameStatus)
+
+        if($scope.gameStatus == "live") {
+          $scope.gamePeriod = TopicService.getGamePeriod();
+          $scope.gameClock = TopicService.getGameClock();
+        }
+
+        $scope.gameScheduledTime = TopicService.getGameTime();
+
+
+
+        $scope.allScoresTitle = TopicService.getScoresTitle();
+        $scope.allScoresURL = TopicService.getScoresLink();
+
+        // renderScoreCard($scope.leftTeam.pColor, $scope.rightTeam.pColor);
+        var left = $('.scoreCardContent');
+        var right = $('.svg-content');
+        left.css('background-color', $scope.leftTeam.pColor);
+        right.css('fill', $scope.rightTeam.pColor);
+      }
+      $scope.topicTitle = TopicService.getTitle();
+      var thisTopic = TopicService.getTopic();
+      $scope.topicDescHtml = thisTopic.html;
+      //console.log("has MEDIA  :"+ thisTopic.type +"thisTopic.mediaUrl"+ thisTopic.mediaUrl);
+      if(thisTopic.type == "media"){
+      $scope.topicMediaUrl = thisTopic.mediaUrl;
+      $scope.topicMediaAspectFeed = thisTopic.mediaAspectFull;
+      }
+//      var width = window.innerWidth;
+//      console.log("Image width :"+$scope.topicMediaAspectFeed.w +"  X  " + $scope.topicMediaAspectFeed.h + "xxxx "+width)
+//      $scope.topicMediaAspectFull = thisTopic.mediaAspectFull;
+
+      $scope.createdAt = TopicService.getTimeCreatedAt();
+      $scope.liked = TopicService.getLiked();
+      var metrics = TopicService.getMetrics();
+      $scope.likesCount = metrics.likes;
+      $scope.commentsCount = metrics.comments;
+      if (!$scope.commentsCount || $scope.commentsCount === 0){
+        $scope.loadingChat = false;
+      }
+
+    }
+  };
+
+  var updateComments = function(){
+    var commentsdata = CommentService.comments();
+    if(commentsdata != undefined && (commentsdata.length >0 || lastComment === true)){
+      lastComment = false;
+      console.log("CommentsData : ", commentsdata);
+      var len = commentsdata.length;
+
+      $scope.commentsArray = [];
+
+      for(i=0;i<len;i++){
+        var tempComment = {};
+        tempComment = commentsdata[i];
+        tempComment.postAuthorName = commentsdata[i].author.name;
+        tempComment.postAuthorPhoto = commentsdata[i].author.photo;
+        tempComment.isMyComment = UserInfoService.isCurrentUser(commentsdata[i].author.id);
+
+        tempComment.likeCount = commentsdata[i].metrics.likes;
+        tempComment.replyCount = commentsdata[i].metrics.replies;
+
+        tempComment.postTimestamp = commentsdata[i].createdAt;
+        if (tempComment.type === 'media'){
+          tempComment.mediaAspectFeed = commentsdata[i].mediaAspectFeed;
+          tempComment.mediaAspectFull = commentsdata[i].mediaAspectFull;
+          tempComment.mediaThumbUrl = commentsdata[i].mediaThumbUrl;
+        }
+        tempComment.isLiked = commentsdata[i].signal.like;
+
+        if (tempComment.type === 'embed'){
+          tempComment.shared = true;
+          tempComment.embed = commentsdata[i].embed;
+          tempComment.embed.embedCreatedAt = commentsdata[i].embedCreatedAt;
+          tempComment.embed.embedCreatedAtFull = commentsdata[i].embedCreatedAtFull;
+
+          if (tempComment.providerName === "Twitter"){
+            tempComment.embed.embedLogo = "img/twitterLogo@2x.png";
+          } else {
+            tempComment.embed.embedLogo = commentsdata[i].embed.provider.logo;
+          }
+
+          if (commentsdata[i].embed.type === 'link' && commentsdata[i].embed.playable === true){
+            tempComment.embed.embedHtml = $sce.trustAsHtml(commentsdata[i].embedHtml);
+          }
+        }
+        
+        $scope.commentsArray.push(tempComment);
+
+      }
+    }
+
+  };
+
+  $scope.loadRemainingComments = function() {
+    console.log("LOADING REST OF COMMENTS...");
+    if (!CommentService.loadedComments()){
+      networkService.send(CommentService.getCommentsRequest($routeParams.topicID));
+      CommentService.setLoadedComments(true);
+      $scope.loadedAllComments = true;
+    }
+  };
+
+  $scope.loadRemainingCommentsTimeout = function() {
+      $timeout(function(){
+        if (!CommentService.loadedComments()){
+          console.log("LOADING REST OF COMMENTS...");
+          networkService.send(CommentService.getCommentsRequest($routeParams.topicID));
+          $scope.loadedAllComments = true;
+          CommentService.setLoadedComments(true);
+        }
+      }, 7000);
+  };
+
+  $scope.init = function() {
+    networkService.send(TopicService.getTopicRequest($routeParams.topicID));
+    networkService.send(CommentService.getCommentsRequest($routeParams.topicID));
+  };
+  
+
+  $scope.setPeelUI = function(isPeelUser){
+    //console.log("isPeelUser :"+isPeelUser);
+    $scope.isPeelUser = isPeelUser;
+    
+  }
+
+  
+  $scope.setPeelUI($scope.isPeelUser);
+
+  $scope.hideLoading = function(){
+    console.log("HIDING LOAD");
+    $scope.loadingChat = false;
+    $scope.loadingSocial = false;
+  };
+  $scope.initPage = function(){
+    updateTopic();
+    updateComments();
+    $scope.pageClass = 'page-topic';
+    $scope.showNewCommentsIndicator = false;
+
+    $scope.topicID = $routeParams.topicID;
+    $scope.init();
+
+    if ($scope.mobileBrowser === true){
+      document.getElementById('topicSection').style.paddingBottom = "42px";
+    }
+  }
+
+  $scope.setLinksOnComments = function(){
+    var postDivs = document.getElementsByClassName("postRow");
+    for (div in postDivs) {
+      var thisDiv = postDivs[div];
+      thisDiv.onclick = function(e) {
+        if ($(e.target).is('a')) {
+          console.log("EXTERNAL LINK: ", e, this.id);
+          return;
+        } 
+        thisPost = $scope.commentsArray[this.id];
+        if ($scope.innerButtonTapped === false) {
+          console.log("Post Click Active: ", thisPost.id);
+          if (HTML5_LOC){
+            $location.path("/post/" + thisPost.id);
+            if (!$scope.$$phase){
+              $scope.$apply();
+            }
+          } else {
+            $window.location = "#/post/" + thisPost.id;
+          }
+        }
+        $scope.innerButtonTapped = false;
+      }
+    }
+  }
+
+//  if(URIHelper.isPeelUser())
+//    ga('send', 'event', 'UserType', '0', 'Peel User', { 'nonInteraction': 2 });
+//  else
+//    ga('send', 'event', 'UserType', '0', 'Non Peel User', { 'nonInteraction': 2 });
+  if(UserInfoService.isUserLoggedIn()){
+    if(NETWORK_DEBUG)
+      console.log("User is logged in, checking for connection");
+    if(!networkService.isSocketConnected())
+      networkService.init();
+    $scope.initPage();
+  }
+  else if (URIHelper.isSmartStadiumUser()){
+    $scope.isSmartStadiumUser = true;
+    console.log("SS User? ", $scope.isSmartStadiumUser);
+    AuthService.loginWithEmail();
+  }
+  else if (URIHelper.isTechMUser()){
+    $window.location = "#/login?MI16=true";
+  }
+  else if (URIHelper.isPeelUser()){
+    $scope.isPeelUser = true;
+    $scope.setPeelUI(true);
+    AuthService.loginWithPeel();
+  }
+  else {
+    // console.log("Not logged in to facebook, take user to login page")
+    if (HTML5_LOC){
+      $location.path("/login");
+    } else {
+      $window.location = "#/login";
+    }
+  }
+
+
+  $scope.peelClose = function()
+  {
+    ga('send', 'event', 'Peel', 'click', 'BackToPeelHome');
+    console.log("peelClose()");
+    window.location = "peel://home";
+  }
+
+  $scope.peelWatchOnTV = function()
+  {
+    ga('send', 'event', 'Peel', 'click', 'PeelWatchOnTV');
+    console.log("peelWatchOnTV()");
+    var showId = URIHelper.getPeelShowId();
+    console.log("Peel show on TV uri :  "+ "peel://tunein/"+showId);
+    if(showId != undefined)
+      window.location = "peel://tunein/"+showId;
+    else
+      window.location = "peel://home";
+  }
+
+  $scope.showNewCommentsIndicator = false;
+  $scope.newCommentsIndicatorTapped = function()
+  {
+    console.log("newCommentsIndicatorTapped");
+    $scope.showNewCommentsIndicator = false;
+    updateComments();
+    $(document).scrollTop(0);
+  }
+
+  $scope.imageClick = function(imageURL)
+  {
+    event.cancelBubble = true;
+    if(event.stopPropagation) event.stopPropagation();
+
+    $.magnificPopup.open({
+      items: {
+        type:'image',
+        src: imageURL,
+      },
+      type: 'inline',
+      callbacks:
+      {
+        open: function()
+        {
+          console.log("popup opened");
+          $('body').bind('touchmove', function(e){e.preventDefault()})
+        },
+        close: function()
+        {
+          console.log("popup closed");
+          $('body').unbind('touchmove')
+        }
+        // e.t.c.
+      }
+    });
+  }
+
+  $scope.moreButtonTapped = function()
+  {
+    $scope.innerButtonTapped = true;
+  }
+
+  $scope.updateLikeTopic = function() {
+    console.log("TopicController update like Topic");
+    if(TopicService.getLiked() == true)
+      networkService.send(TopicService.getUnlikeTopicRequest());
+    else
+      networkService.send(TopicService.getLikeTopicRequest());  
+  };
+
+  $scope.commentOnTopic = function()
+  {
+    // console.log("comment on topic");
+    document.getElementById("topicCommentField").focus();
+  };
+
+  $scope.updateLikeComment = function(id) {
+    $scope.innerButtonTapped = true;
+    
+    // event.cancelBubble = true;
+    // if(event.stopPropagation) event.stopPropagation();
+
+    console.log("TopicController updateLike (" + id + ")");
+    if(CommentService.isCommentLiked(id)){
+      networkService.send(CommentService.getUnlikeCommentRequest(id));
+    }
+    else{
+      networkService.send(CommentService.getLikeCommentRequest(id));  
+    }
+  };
+
+
+  $scope.deleteComment = function(id)
+  {
+    console.log("deleteComment(" + id + ")");
+    if ($scope.commentsArray.length === 1){
+      console.log("Deleting Final Comment");
+      lastComment = true;
+    }
+    $scope.innerButtonTapped = true;
+    networkService.send(CommentService.deleteCommentRequest(id));
+  }
+
+  $scope.reportCommentAsSpam = function(id)
+  {
+    console.log("reportCommentAsSpam(" + id + ")");
+    $scope.innerButtonTapped = true;
+    networkService.send(CommentService.flagCommentRequest(id)); 
+  }
+
+  $scope.goToRepliesWithKeyboardTriggered = function(id)
+  {
+    // event.cancelBubble = true;
+    // if(event.stopPropagation) event.stopPropagation();
+
+    // console.log("TopicController.goToRepliesWithKeyboardTriggered(" + id + ")");
+    TopicService.directComment = true;
+    if (HTML5_LOC){
+      $location.path("/post/" + id);
+    } else {
+      $window.location = "#/post/" + id;
+    }
+  };
+
+
+
+  var notifyNewComments = function(){
+    if($scope.commentsArray == undefined)
+    {
+      updateComments();
+    }
+    else {
+      var commentsdata = CommentService.comments();
+      var len = commentsdata.length;
+      var pinIndex = CommentService.getNumPinComments();
+      if($scope.commentsArray.length < len){
+        if(!UserInfoService.isCurrentUser(commentsdata[pinIndex].author.id)){
+          $scope.showNewCommentsIndicator = true;
+        }
+        else {
+          updateComments();
+        }
+      }
+      else{
+        updateComments();
+      }
+    }
+  };
+
+  TopicService.registerObserverCallback(updateTopic);
+  CommentService.registerObserverCallback(notifyNewComments);
+  CommentService.registerObserverCallback(updateComments, true);
+
+  $scope.trustSrc = function(src)
+  {
+    return $sce.trustAsResourceUrl(src);
+  }
+
+  $window.addEventListener("beforeunload", function(){
+    console.log("Before Unload");
+    ForumStorage.setToLocalStorage("lastTabActive", $scope.activeTab);
+  });
+
+  $scope.xLinkActivated = false;
+
+  // CONTENT TABS
+  $scope.activeTab = 'chat';
+  $scope.switchTabs = function(tab) {
+    if (tab === 'chat'){
+      $('#chatTab').addClass('selectedTab');
+      $('#videoTab').removeClass('selectedTab');
+      $('#socialTab').removeClass('selectedTab');
+      $scope.activeTab = 'chat';
+      $(document).scrollTop(0);
+      updateTopic();
+      updateComments();
+    }
+    if (tab === 'video'){
+      $('#chatTab').removeClass('selectedTab');
+      $('#videoTab').addClass('selectedTab');
+      $('#socialTab').removeClass('selectedTab');
+      $scope.activeTab = 'video';
+      $(document).scrollTop(0);
+    }
+    if (tab === 'social'){
+      $('#chatTab').removeClass('selectedTab');
+      $('#videoTab').removeClass('selectedTab');
+      $('#socialTab').addClass('selectedTab');
+      $scope.activeTab = 'social';
+      $(document).scrollTop(0);
+    }
+    console.log("Active Tab: ", $scope.activeTab);
+  };
+
+  var _channelId = ChannelService.getChannel();
+  TopicService.setChannel(_channelId);
+
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  var docVarsSet = false;
+  var tabs,
+      tabContainer,
+      tabsTop,
+      tabsHeight,
+      inputHeight,
+      clientHeight,
+      docHeight,
+      headerHeight;
+  $scope.setDocVars = function() {
+    if (!docVarsSet){
+      tabs = $('#inputControls');
+      tabContainer = $('.tabContainer');
+      tabsTop = tabs.offset().top;
+      tabsHeight = tabContainer.height();
+      inputHeight = tabs.height();
+      clientHeight = document.documentElement.clientHeight;
+      docHeight = $(document).height();
+      docVarsSet = true;
+      if ($scope.isSmartStadiumUser){
+        headerHeight = 54;
+      } else if ($scope.isPeelUser){
+        headerHeight = 54;
+      } else {
+        headerHeight = 0;
+      }
+    }
+  };
+
+  var fixed = false;
+  var watchScroll = function watchScroll() {
+    console.log("Tabs Top: ", tabsTop);
+    if ($scope.showNewCommentsIndicator){
+      $scope.showNewCommentsIndicator = false;
+    }
+    // if ($scope.isPeelUser){
+      if ($(document).scrollTop() > (tabsTop - headerHeight) && (docHeight - clientHeight) > (tabsTop + inputHeight - tabsHeight)) {
+        tabs.addClass('fixTabsPeel');
+        tabs.css('top',headerHeight);
+        tabContainer.addClass('fixTabContainer');
+        fixed = true;
+      } else if (fixed) {
+        tabs.removeClass('fixTabsPeel');
+        tabs.css('top','');
+        tabContainer.removeClass('fixTabContainer');
+        fixed = false;
+      }
+  };
+
+  $(document).off('scroll');
+  $(document).on('scroll', watchScroll);
+
+
+}]);
+
---
+++ forum-web/js/pre/directives/TopicDirectives.js
@@ -0,0 +1,100 @@
+angular.module('TopicModule')
+.directive('repeatFinishedNotify', function () {
+  return function (scope, element, attrs) {
+    if (scope.$last){
+      // scope.scrollToBookmark();
+      console.log("DONE LOADING COMMENTS");
+      scope.hideLoading();
+      scope.setLinksOnComments();
+      scope.setDocVars();
+      scope.continueToExperience('smartS');
+      // scope.loadRemainingCommentsTimeout();
+    }
+  };
+});
+
+angular.module('TopicModule')
+.directive('embedSharedContent', ['UserAgentService',
+  function (UserAgentService) {
+  return {
+    restrict: 'E',
+    scope: {
+      thisPost: '=embedPost',
+      imageZoom: '&',
+      trustSource: '&',
+      preventNav: '&'      
+    },
+    link: function(scope,elem,attr){
+      scope.mobileUserAgent = UserAgentService.getMobileUserAgent();
+    },
+    templateUrl: 'partials/shared.html'
+  };
+}]);
+
+angular.module('TopicModule')
+.directive('mediaPlayer', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      thisPost: '=',
+      vidSource: '@',
+      vidIndex: '@'
+    },
+    link: function(scope,elem,attr) {
+
+      scope.togglePlayPause = function(e) {
+        var thesePlayerNodes = elem[0].firstElementChild.childNodes;
+        var thisVideo = thesePlayerNodes[1];
+        var thisThumbnail = thesePlayerNodes[3];
+        var thisPlayBtn = thesePlayerNodes[5];
+        console.log("This Player: ", thisVideo, elem, thesePlayerNodes);
+        console.log("This Play Button: ", thisPlayBtn);
+        console.log("This Player Thumbnail: ", thisThumbnail);
+
+        if (thisVideo.paused || thisVideo.ended){
+          console.log("Play");
+          thisPlayBtn.className = 'pause';
+          thisThumbnail.className = 'pause';
+          thisVideo.play();
+        }
+        else {
+          console.log("Pause");
+          thisPlayBtn.className = 'media-controls';
+          thisThumbnail.className = 'media-thumbnail';
+          thisVideo.pause();
+        }
+      }
+
+      scope.setAspectRatio = function (aspectRatio, orientation) {
+        console.log("setAspectRatio: ", aspectRatio, orientation);
+        var classStrings = [];
+        // Set Aspect Ratio Class
+        if (aspectRatio === 1){
+          classStrings.push("thumb1x1");
+        } else if (aspectRatio === 1.778){
+          classStrings.push("thumb16x9");
+        } else if (aspectRatio === 2){
+          classStrings.push("thumb2x1");
+        }
+        // Set Orientation Class
+        if (orientation === "portrait"){
+          classStrings.push("video-portrait");
+        } else {
+          classStrings.push("video-landscape");
+        }
+        return classStrings;
+      }
+
+      scope.getContainerHeight = function (aspectRatio) {
+        var _this = this;
+        var height = document.documentElement.clientWidth / aspectRatio;
+        height = height + 'px';
+        // console.log("This: ", this, height);
+        return height;
+      }
+
+    },
+    templateUrl: 'partials/mediaPlayer.html'
+  };
+});
+
