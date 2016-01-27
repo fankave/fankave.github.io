d58b430acab5af0f857dfe878a1d9ab1844f2948
Robby Hays <rhaysvm42@gmail.com>
2016-01-27 15:03:34 -0800

    Infinite Scrolling Prep

--- js/pre/directives/InfiniteSegment.js
+++ js/pre/directives/InfiniteSegment.js
@@ -2,37 +2,34 @@ angular.module('TopicModule')
 .directive('infiniteSegment', ['$sce', function($sce) {
   return {
     restrict: 'E',
-    scope: {
-      'thisSegment': '='
-    },
     link: function(scope, elem, attrs){
 
-      scope.trustSrc = function(src) {
-          return $sce.trustAsResourceUrl(src);
-      }
+      // scope.trustSrc = function(src) {
+      //     return $sce.trustAsResourceUrl(src);
+      // }
 
-      scope.imageClick = function(imageURL) {
-        event.cancelBubble = true;
-        if(event.stopPropagation) event.stopPropagation();
+      // scope.imageClick = function(imageURL) {
+      //   event.cancelBubble = true;
+      //   if(event.stopPropagation) event.stopPropagation();
 
-        $.magnificPopup.open({
-          items: {
-            type:'image',
-            src: imageURL,
-          },
-          type: 'inline',
-          callbacks: {
-            open: function() {
-              console.log("popup opened");
-              $('body').bind('touchmove', function(e){e.preventDefault()})
-            },
-            close: function() {
-              console.log("popup closed");
-              $('body').unbind('touchmove')
-            }
-          }
-        });
-      }
+      //   $.magnificPopup.open({
+      //     items: {
+      //       type:'image',
+      //       src: imageURL,
+      //     },
+      //     type: 'inline',
+      //     callbacks: {
+      //       open: function() {
+      //         console.log("popup opened");
+      //         $('body').bind('touchmove', function(e){e.preventDefault()})
+      //       },
+      //       close: function() {
+      //         console.log("popup closed");
+      //         $('body').unbind('touchmove')
+      //       }
+      //     }
+      //   });
+      // }
     },
     templateUrl: function(elem, attrs) {
       if (attrs.env === 'chat'){
