e96ecee509919133e776a89548ab1768b7823b0d
Robby Hays <rhaysvm42@gmail.com>
2016-02-11 14:26:41 -0800

    Revised Comment Click

--- js/pre/controllers/TopicController.js
+++ js/pre/controllers/TopicController.js
@@ -315,30 +315,11 @@ function ($scope, $sce, $window, $location, $sanitize, $timeout, $routeParams,ne
     }
   }
 
-  $scope.setLinksOnComments = function(){
-    var postDivs = document.getElementsByClassName("postRow");
-    for (div in postDivs) {
-      var thisDiv = postDivs[div];
-      thisDiv.onclick = function(e) {
-        if ($(e.target).is('a')) {
-          console.log("EXTERNAL LINK: ", e, this.id);
-          return;
-        } 
-        thisPost = $scope.commentsArray[this.id];
-        if ($scope.innerButtonTapped === false) {
-          console.log("Post Click Active: ", thisPost.id);
-          if (HTML5_LOC){
-            $location.path("/post/" + thisPost.id);
-            if (!$scope.$$phase){
-              $scope.$apply();
-            }
-          } else {
-            $window.location = "#/post/" + thisPost.id;
-          }
-        }
-        $scope.innerButtonTapped = false;
-      }
+  $scope.viewPost = function(e,id){
+    if ($(e.target).is('a')){
+      return;
     }
+    $window.location = "#/post/" + id;
   }
 
 //  if(URIHelper.isPeelUser())
--- js/pre/directives/TopicDirectives.js
+++ js/pre/directives/TopicDirectives.js
@@ -51,7 +51,7 @@ angular.module('TopicModule')
       // scope.scrollToBookmark();
       // console.log("DONE LOADING COMMENTS");
       scope.hideLoading();
-      scope.setLinksOnComments();
+      // scope.setLinksOnComments();
       scope.setDocVars();
       scope.continueToExperience('smartS');
       scope.showLoadMore();
