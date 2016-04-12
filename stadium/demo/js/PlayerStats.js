269236bfbfe8c2845144e9b82846c9ca1de92acd
Robby Hays <rhaysvm42@gmail.com>
2016-04-11 22:54:11 -0700

    Player Stats Directive Refactor + Count Up

--- stadium/demo/js/PlayerStats.js
+++ stadium/demo/js/PlayerStats.js
@@ -4,27 +4,58 @@ function ($http, $timeout) {
 
   var _this = this;
 
-  function barUp (id, dur, height) {
-    var $elem = $('#bar' + id);
-    var $player = $('#player' + id);
-    var heights = ['-32px','-64px','-120px'];
-    var dur = dur || 1500;
-    $elem
-    .css({ visibility: 'visible' })
-    .animate({ height: height + 'px' }, dur, 'swing', function(){
-      $player.animate({ top: heights[id-1] }, 200);
-      if (id === 1){
-        $('#trail1').animate({ top: '130px' }, 200);
-      }
-      else if (id === 3){
-        $('#trail3').animate({ top: '-20px' }, 200);
-      }
-    });
+  function roundToTenths (num) {
+    return Math.max(Math.round(num * 10) / 10).toFixed(1);
   }
-  $timeout(function(){
-    barUp(1, 1500, 592);
-    barUp(2, 1500, 720);
-    barUp(3, 1500, 520);
-  }, 1000);
 
-}]);
+  $.fn.countUp = function (duration, easing, complete) {
+    return this.each(function () {
+      var $this = $(this);
+      var stepFn;
+      if ($this.text().indexOf('.') === -1) {
+        stepFn = function(num){return Math.ceil(num);};
+      } else {
+        stepFn = function(num){return Math.max(Math.round(num * 10) / 10).toFixed(1);};
+      }
+      $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+        duration: duration,
+        easing: easing || 'swing',
+        step: function () {
+          $this.text(stepFn(this.Counter));
+        },
+        complete: complete || $.noop
+      });
+    });
+  };
+
+}])
+.directive('barUp', function() {
+  return {
+    restrict: 'A',
+    link: function (scope, elem, attrs) {
+      var $player = elem.find('img');
+      var $trail = elem.find('.trail');
+      console.log('barUp: ', $player, $trail);
+      $(elem)
+      .css({ visibility: 'visible' })
+      .animate({ height: attrs.barUp }, {
+        duration: 1900,
+        easing: 'swing',
+        complete: function () {
+          $player.animate({ top: attrs.playerEnd }, 200);
+          if (!!attrs.trailEnd) {
+            $trail.animate({ top: attrs.trailEnd }, 200);
+          }
+        }
+      });
+    }
+  };
+})
+.directive('countUp', function () {
+  return {
+    restrict: 'A',
+    link: function (scope, elem, attrs) {
+      $(elem).countUp(1900);
+    }
+  }
+});
